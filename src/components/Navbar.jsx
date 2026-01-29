8bb20322e08854ab4417f8eab266874bcdf3c549
devmashud <2345arham@gmail.com>
2026-01-29 22:37:47 +0600

    mananger componnent add and backgound add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between px-4 bg-pink-200 '>
+    <nav className='flex items-center justify-around px-4 bg-pink-200 h-13'>
         <div className="logo font-bold">KeyRoom</div>
         <ul>
             <li className='flex gap-4'>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
+                <a className='hover:font-bold' href="/">Home</a>
+                <a className='hover:font-bold' href="/about">About</a>
             </li>
         </ul>
     </nav>
